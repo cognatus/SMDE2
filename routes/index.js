3c4c135f617b65ee424d769cf80824536ee05b3c
cognatus <diegojmartinezzm@gmail.com>
2016-10-16 20:17:25 -0500

    Aun hace falta arreglar weas, pero ya casí

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('pos hay algo aqui');
-});
+
 
 module.exports = router;
