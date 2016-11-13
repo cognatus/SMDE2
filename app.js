9d6bc15caeecccad4d7142e85d6bfaba6624d188
AlexVanHell <alejandrodnl.cv@hotmail.com>
2016-11-12 23:08:53 -0600

    Moviendo de www a app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var mysql = require('mysql');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,6 +16,13 @@ app.use(cors());
 var api = require('./routes/index');
 var users = require('./routes/users');
 
+var post = require('./api/post');
+var agenda = require('./api/agenda');
+var perfil = require('./api/perfil');
+var asignaturas = require('./api/asignaturas');
+var mensajes = require('./api/mensajes');
+var foro = require('./api/foro');
+
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -26,6 +34,38 @@ app.use(favicon(__dirname + '/icon.png'));
 
 app.use('/api', api);
 app.use('/users', users);
+
+// Conecta a la DB MySQL
+function databaseInstance(){
+  var connection = mysql.createConnection({
+    multipleStatements: true,
+    host: 'localhost',
+    password: 'n0m3l0',
+    user: 'root',
+    database: 'smdedbv1',
+    port: 3306
+  });
+  return connection;
+};
+
+// Este es el fucking constructor xD
+
+/*function(){
+  var base = new databaseInstance();
+  base.connect(function(error){
+    if(error){
+      throw error;
+    }else{
+      console.log('Conexion correcta.');
+    }
+  });
+  post.constructor(databaseInstance);
+  perfil.constructor(databaseInstance);
+  agenda.constructor(databaseInstance);
+  asignaturas.constructor(databaseInstance);
+  mensajes.constructor(databaseInstance);
+  foro.constructor(databaseInstance);
+}*/
 
 /*
   Esta wea es para poder auntentificar con Angular 2
