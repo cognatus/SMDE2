f1a162a553cee82347de923d047d86fc4791689e
AlexVanHell <alejandrodnl.cv@hotmail.com>
2017-05-20 19:26:16 -0500

    Corrigiendo users.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,23 +1,23 @@
 export class User {
-	id: string,
-	mail: string,
-	password: string,
-	name: string,
-	lastName: string,
-	phone: number,
-	birthDay: string,
-	userType: number,
-	sex: number,
+	id: string;
+	mail: string;
+	password: string;
+	name: string;
+	lastName: string;
+	phone: number;
+	birthDay: string;
+	userType: number;
+	sex: number;
 	courses: [{
 		subject: {
-			id: string,
-			level: number,
-			name: string,
-		},
+			id: string;
+			level: number;
+			name: string;
+		};
 		group: {
-			id: string,
-			level: number,
-			name: string,
+			id: string;
+			level: number;
+			name: string;
 		}
-	}]
-}
+	}];
+};
